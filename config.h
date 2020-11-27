15ec443c0ce8b9e9960f391305fd21c5a6b99eb3
Waldemar Schroeer <w@ldemar.de>
2020-11-27 22:58:29 +0100

    Initial commit.

---
+++ config.h
@@ -0,0 +1,10 @@
+//Enable this option if you want use syslog daemon for logging
+#define USE_SYSLOG
+//Default directory for dump mail from mail server
+#define DEFAULT_DUMPMAILDIR "/var/maildump"
+//Default full file name for LOCK file
+#define DEFAULT_DUMPMAIL_LOCK "/var/run/maildump.pid"
+//Version of this program 
+#define VERSION "RZ-Amper MailArchiver Ver. 2.1, 2013"
+//PREFIX for SQLTable
+#define SQLTABLE_PREFIX "mailarchive"
